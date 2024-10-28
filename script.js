24e06c02560293c8414365a6f22021b7741d170c
Soniya <nonugpt0211@gmail.com>
2024-10-28 16:22:31 +0530

    RLP

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function toggleMenu() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+}
